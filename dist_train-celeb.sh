71c8e025b293556ad6333f62f525f6bdf071a57b
Yewon <ga06033@yonsei.ac.kr>
2024-05-14 11:45:15 -0400

    Delete dist_train-celeb.sh

--- dist_train-celeb.sh
+++
@@ -1,5 +0,0 @@
-EXP_NAME="y1_celebahq_distill_naive_mse_ratio_1_1_augfixed"
-DATASETS="celebahq"
-
-
-torchrun --standalone --nproc-per-node=4 --master_port=25678 train.py --gpus 4,5,6,7 --exp_name $EXP_NAME --datasets $DATASETS 
