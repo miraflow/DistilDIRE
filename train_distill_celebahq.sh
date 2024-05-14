1e1280cddbc1730110f9ba5f63d80d0138a13046
Yewon <ga06033@yonsei.ac.kr>
2024-05-14 11:42:49 -0400

    Delete train_distill_celebahq.sh

--- train_distill_celebahq.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-### make sure that you have modified the EXP_NAME, DATASETS, DATASETS_TEST
-#eval "$(conda shell.bash hook)"
-# conda activate dire
-EXP_NAME="celebahq_distill_naive_mse_ratio_1_1"
-DATASETS="celebahq"
-#DATASETS_TEST="celebahq"
-python3 train.py --gpus 4,5,6,7 --exp_name $EXP_NAME --datasets $DATASETS #datasets_test $DATASETS_TEST
