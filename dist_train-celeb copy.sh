aff319a7cd9d834bd14748b4059d9494a7f9d84f
Yewon <ga06033@yonsei.ac.kr>
2024-05-14 11:45:24 -0400

    Delete dist_train-celeb copy.sh

--- dist_train-celeb copy.sh
+++
@@ -1,5 +0,0 @@
-EXP_NAME="y1_imagenet_distill_naive_mse_ratio_1_1"
-DATASETS="imagenet"
-
-
-torchrun --standalone --nproc-per-node=4 train.py --gpus 0,1,2,3 --exp_name $EXP_NAME --datasets $DATASETS
