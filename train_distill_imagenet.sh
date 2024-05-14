af8c0bccaa7f3645c26458b934763c4e4be49036
Yewon <ga06033@yonsei.ac.kr>
2024-05-14 11:43:16 -0400

    code cleanup

--- train_distill_imagenet.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-### make sure that you have modified the EXP_NAME, DATASETS, DATASETS_TEST
-#eval "$(conda shell.bash hook)"
-# conda activate dire
-EXP_NAME="imagenet_distill_naive_mse_ratio_1_1"
-DATASETS="imagenet"
-#DATASETS_TEST="celebahq"
-python3 train.py --gpus 0,1,2,3 --exp_name $EXP_NAME --datasets $DATASETS #datasets_test $DATASETS_TEST
